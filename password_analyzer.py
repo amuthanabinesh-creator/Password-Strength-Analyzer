12da4c41099b5612e83231e880387a77ad591740
Amuthan Abinesh <amuthanabinesh@gmail.com>
2026-06-08 18:21:12 +0530

    Initial commit

---
+++ password_analyzer.py
@@ -0,0 +1,59 @@
+import re
+
+def check_password_strength(password):
+    score = 0
+    suggestions = []
+
+    # Length Check
+    if len(password) >= 8:
+        score += 1
+    else:
+        suggestions.append("Use at least 8 characters.")
+
+    # Uppercase Check
+    if re.search(r"[A-Z]", password):
+        score += 1
+    else:
+        suggestions.append("Add uppercase letters.")
+
+    # Lowercase Check
+    if re.search(r"[a-z]", password):
+        score += 1
+    else:
+        suggestions.append("Add lowercase letters.")
+
+    # Number Check
+    if re.search(r"\d", password):
+        score += 1
+    else:
+        suggestions.append("Add numbers.")
+
+    # Special Character Check
+    if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+        score += 1
+    else:
+        suggestions.append("Add special characters.")
+
+    # Strength Rating
+    if score <= 2:
+        strength = "Weak"
+    elif score == 3 or score == 4:
+        strength = "Medium"
+    else:
+        strength = "Strong"
+
+    return strength, suggestions
+
+
+password = input("Enter a password: ")
+
+strength, suggestions = check_password_strength(password)
+
+print("\nPassword Strength:", strength)
+
+if suggestions:
+    print("\nSuggestions to improve:")
+    for suggestion in suggestions:
+        print("-", suggestion)
+else:
+    print("Excellent password!")
